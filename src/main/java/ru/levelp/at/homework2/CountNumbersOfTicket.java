7bd864c576c6e5d2678933d1a3876be8849fc792
SYuIvchenko <syuivchenko@beeline.ru>
2022-09-30 23:55:38 +0300

    "feat: draft countNumbers"

---
+++ src/main/java/ru/levelp/at/homework2/CountNumbersOfTicket.java
@@ -0,0 +1,15 @@
+package ru.levelp.at.homework2;
+
+import java.util.List;
+
+public class CountNumbersOfTicket {
+    public boolean countNumbers(List<Integer> number){
+        boolean result = false;
+        if(number.get(1)+number.get(2)+number.get(3)==number.get(4)+number.get(5)+number.get(6)){
+            result=true;
+        }
+
+        return result;
+
+    }
+}
