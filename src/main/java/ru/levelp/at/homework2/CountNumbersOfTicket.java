031a5d831cb93b4caa8a4c473d8a328739a8d305
SYuIvchenko <syuivchenko@beeline.ru>
2022-10-03 00:08:29 +0300

    "fix: add suits"

--- src/main/java/ru/levelp/at/homework2/CountNumbersOfTicket.java
+++ src/main/java/ru/levelp/at/homework2/CountNumbersOfTicket.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class CountNumbersOfTicket {
 
-    public boolean countNumbers(List<Integer> number){
+    public boolean countNumbers(List<Integer> number) {
         boolean result = false;
         try {
             if (number.get(0) + number.get(1) + number.get(2) == number.get(3) + number.get(4) + number.get(5)) {
--- src/test/java/ru.levelp.at.homework2/CountNumbersOfTicketTest.java
+++ src/test/java/ru.levelp.at.homework2/CountNumbersOfTicketTest.java
@@ -1,42 +1,40 @@
 package ru.levelp.at.homework2;
 
+import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-
-import java.util.List;
-
 public class CountNumbersOfTicketTest {
-    private static final List<Integer> INPUT = List.of(1,2,3,1,2,3);
+    private static final List<Integer> INPUT = List.of(1, 2, 3, 1, 2, 3);
     private static final Boolean RESULT = true;
     private CountNumbersOfTicket testCheck;
 
     @BeforeMethod
-    public void beforeMethod(){
+    public void beforeMethod() {
         testCheck = new CountNumbersOfTicket();
     }
 
     @Test
     //проверка любых успешных с разными корректным входными данными (a=b, a!=b, 0)
-    public void inputNumbersPositive(){
+    public void inputNumbersPositive() {
         boolean actualOutput = testCheck.countNumbers(INPUT);
         Assertions.assertThat(actualOutput).isEqualTo(RESULT);
 
     }
 
     @Test
-    public void inputNullNegative(){
+    public void inputNullNegative() {
         boolean actualOutput = testCheck.countNumbers(null);
         Assertions.assertThat(actualOutput).isEqualTo(RESULT);
 
     }
 
     @Test
-    public void inputNotFullNumbersNegative(){
+    public void inputNotFullNumbersNegative() {
         boolean actualOutput = testCheck.countNumbers(null);
         Assertions.assertThat(actualOutput).isEqualTo(RESULT);
-
+        //доделать параметры
     }
 
 }
