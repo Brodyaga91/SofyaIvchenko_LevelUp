29a444b4e926009bad0c75bbdb646e8346bdd56c
SYuIvchenko <syuivchenko@beeline.ru>
2022-10-11 21:09:52 +0300

    "fix: fix comment"

--- src/main/java/ru/levelp/at/homework2/CountNumbersOfTicket.java
+++ src/main/java/ru/levelp/at/homework2/CountNumbersOfTicket.java
@@ -1,25 +1,22 @@
 package ru.levelp.at.homework2;
 
-import java.util.List;
 
 public class CountNumbersOfTicket {
 
-    public boolean countNumbers(List<Integer> number) {
+    public boolean countNumbers(String numberOfTicket) {
         boolean result = false;
-
         try {
-            if (number.get(0) + number.get(1) + number.get(2) == number.get(3) + number.get(4) + number.get(5)) {
+            char[] arrayOfChars = numberOfTicket.toCharArray();
+            int[] arrayOfNumbers = new int[6];
+
+            for (int i = 0; i < 6; i++) {
+                arrayOfNumbers[i] = Character.getNumericValue(arrayOfChars[i]);
+            }
+            if (arrayOfNumbers[0] + arrayOfNumbers[1] + arrayOfNumbers[2] == arrayOfNumbers[3] + arrayOfNumbers[4]
+                + arrayOfNumbers[5]) {
                 result = true;
             }
-            for (int a : number) {
-                a = 0;
-                if (number.get(a) < 0) {
-                    result = false;
 
-                }
-                break;
-
-            }
         } finally {
             return result;
         }
--- src/test/java/ru.levelp.at.homework2/CountNumbersOfTicketTest.java
+++ src/test/java/ru.levelp.at.homework2/CountNumbersOfTicketTest.java
@@ -1,6 +1,5 @@
 package ru.levelp.at.homework2;
 
-import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
@@ -17,31 +16,31 @@ public class CountNumbersOfTicketTest {
     @DataProvider
     public Object[][] checkHappyDataProvider() {
         return new Object[][] {
-            {List.of(1, 2, 3, 4, 5, 6), false},
-            {List.of(1, 2, 3, 1, 2, 3), true},
-            {List.of(0, 0, 0, 0, 0, 0), true}
+            {"123456", false},
+            {"123123", true},
+            {"000000", true}
         };
     }
 
     @DataProvider
     public Object[][] checkHappyDataProviderNegative() {
         return new Object[][] {
-            {List.of(1, 2, 3, 4, 5, 6, 7), false},
-            {List.of(1, 2, 3, 1, 2), false},
-            {List.of(-1, -2, -3, -1, -2, -3), false},
-            {List.of("a", "a", "a", "a", "a", "a"), false},
+            {"1234567", false},
+            {"12312", false},
+            {"-1-2-3-1-2-3)", false},
+            {"jsdfls", false},
             {null, false}
         };
     }
 
     @Test(dataProvider = "checkHappyDataProvider")
-    public void inputNumbers(List<Integer> input, boolean result) {
+    public void inputNumbers(String input, boolean result) {
         boolean actualOutput = testCheck.countNumbers(input);
         Assertions.assertThat(actualOutput).isEqualTo(result);
     }
 
     @Test(dataProvider = "checkHappyDataProviderNegative")
-    public void inputNumbersBad(List<Integer> input, boolean result) {
+    public void inputNumbersBad(String input, boolean result) {
         boolean actualOutput = testCheck.countNumbers(input);
         Assertions.assertThat(actualOutput).isEqualTo(result);
     }
